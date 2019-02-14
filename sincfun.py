658a77369108f5ef4a174e0169bab4161d8bd969
Dayalan Saravanan <dayalsaravanan@gmail.com>
2019-02-14 17:00:47 +0530

    script to plot normalized sinc function

--- sincfun.py
+++ sincfun.py
@@ -2,21 +2,31 @@
 # File: sincfun.py
 # Name: D.Saravanan
 # Date: 24/05/2021
-# Script to plot normalized sinc function
 
-import math
-import matplotlib.pyplot as plt
+""" Script to plot normalized sinc function """
+
+import numpy as np
 import matplotlib
+from matplotlib import ticker
+import matplotlib.pyplot as plt
 matplotlib.use('Agg')
 
-def sinc(value: float) -> float:
+def sinc(x: float) -> float:
     """ sinc function """
-    return math.sin(math.pi * value)/(math.pi * value)
+    return np.sin(np.pi * x)/(np.pi * x)
 
-xval, yval = [], []
-for nval in range(1,101):
-    xval.append(nval)
-    yval.append(sinc(nval))
+x = np.arange(-2*np.pi, 2*np.pi, 0.001)
+#x, y = [n, sinc(n) for n in np.arange(-2*np.pi, 2*np.pi, 0.001)]
+y = [sinc(n) for n in x]
 
-plt.plot(xval, yval)
+ax = plt.axes()
+ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
+ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+
+plt.plot(x, y, label=r'$\frac{\sin(\pi x)}{\pi x}$')
+plt.title('sinc function', fontsize=10.)
+plt.xlabel('x'); plt.ylabel(r'$\sin(\pi x)/\pi x$')
+plt.grid(True, which='both'); plt.legend()
 plt.savefig('sincfun.png')
