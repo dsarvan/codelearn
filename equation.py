930a247f62d6bfb0c7c7024961cc914f88280687
Dayalan Saravanan <dayalsaravanan@gmail.com>
2019-02-14 18:24:42 +0530

    script to find solutions to equation x**2 - 7 y**2 = 1

--- equation.py
+++ equation.py
@@ -3,11 +3,12 @@
 # Name: D.Saravanan
 # Date: 10/10/2020
 
-""" Script to find solutions to equation (x**2) - 7*(y**2) = 1 """
+""" Script to find solutions to equation x**2 - 7 y**2 = 1 """
 
-import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use('seaborn-dark')
+import numpy as np
+
+plt.style.use("seaborn-dark")
 
 solutions = []
 
@@ -15,17 +16,17 @@ s1, s2 = -10000, 10000
 
 for x in np.arange(s1, s2, 1):
     for y in np.arange(s1, s2, 1):
-        if (x**2) - 7*(y**2) == 1:
+        if (x ** 2) - 7 * (y ** 2) == 1:
             solutions.append((x, y))
 
 print(solutions)
 
-#xval = [x[0] for x in solutions]
-#yval = [x[1] for x in solutions]
+# xval = [x[0] for x in solutions]
+# yval = [x[1] for x in solutions]
 
 xval, yval = zip(*solutions)
 
-plt.plot(xval, yval, ':')
-plt.plot(xval, yval, 'ob')
+plt.plot(xval, yval, ":")
+plt.plot(xval, yval, "ob")
 plt.grid(True)
-plt.savefig('equation.png')
+plt.savefig("equation.png")
