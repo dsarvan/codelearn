9206403c1fe12bbe3371df8573787f5bf4beb5f3
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-09-01 22:41:27 +0530

    script to configure hadoop as root user

--- hadoopconf.sh
+++ hadoopconf.sh
@@ -4,16 +4,20 @@
 apt-get update
 apt-get install -y sudo
 
-# add user
+# add user and add user to group sudo with bash shell
 user="raman"
 useradd -m -s /bin/bash -G sudo $user
 
 # ssh configuration
-sudo apt-get update
-sudo apt-get install -y vim openssh-server ssh
+apt-get update
+apt-get install -y vim openssh-server ssh
 mkdir /home/$user/.ssh/
-sudo ssh-keygen -t rsa -P " " -f "/home/$user/.ssh/id_rsa" -q
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+ssh-keygen -t rsa -P " " -f "/home/$user/.ssh/id_rsa" -q
+cat /home/$user/.ssh/id_rsa.pub >> /home/$user/.ssh/authorized_keys
+
+# start ssh
+service ssh start
+#systemctl start ssh
 
 # vi configuration
 touch /home/$user/.exrc
@@ -29,12 +33,8 @@ set showmatch
 set smartcase
 set tabstop=4
 highlight LineNr cterm=NONE ctermfg=DarkGrey ctermbg=NONE" >> /home/$user/.exrc
-
-# start ssh
-# sysVinit
-sudo service ssh start
-# systemd
-#sudo systemctl start ssh
+chown $user:$user /home/$user/.exrc
+source /home/$user/.exrc
 
 # installing openjdk8
 # updating the packages list
@@ -53,10 +53,10 @@ echo "JAVA_HOME=/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64" >> /etc/environment
 source /etc/environment
 
 # hadoop configuration
-mkdir /home/$user/Downloads
-wget -C https://archive.apache.org/dist/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz /home/$user/Downloads
-tar -xzf /home/$user/Downloads/hadoop-3.2.1.tar.gz
-mv /home/$user/Downloads/hadoop-3.2.1/ /usr/local/hadoop/ 
+wget -c https://archive.apache.org/dist/hadoop/common/hadoop-3.2.1/hadoop-3.2.1.tar.gz
+tar -xzf hadoop-3.2.1.tar.gz
+chown -R $user:$user hadoop-3.2.1/
+mv hadoop-3.2.1/ /usr/local/hadoop/ 
 
 # set environment variables
 echo "PATH=/usr/local/hadoop/bin:/usr/local/hadoop/sbin:$PATH" >> /home/$user/.profile
@@ -70,25 +70,23 @@ export HADOOP_MAPRED_HOME=$HADOOP_HOME
 export HADOOP_YARN_HOME=$HADOOP_HOME
 export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
 export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin" >> /home/$user/.bashrc
-source .bashrc
+source /home/$user/.bashrc
 
-# configure the node
-# number of replications
-replica=2
+# configuration of the node
 
 # hdfs-site.xml
-sed -i "s/<configuration>/& \
-\n\t<property> \
+sed -i 's/<configuration>/& \
+\t<property> \
 \t\t<name>dfs.namenode.name.dir<\/name> \
-\t\t<value>/usr/local/hadoop/hdfs/nameNode<\/value> \
+\t\t<value>\/usr\/local\/hadoop\/hdfs\/nameNode<\/value> \
 \t<\/property> \
 \t<property> \
 \t\t<name>dfs.datanode.data.dir<\/name> \
-\t\t<value>/usr/local/hadoop/hdfs/dataNode<\/value> \
+\t\t<value>\/usr\/local\/hadoop\/hdfs\/dataNode<\/value> \
 \t<\/property> \
 \t<property> \
 \t\t<name>dfs.replication<\/name> \
-\t\t<value>$replica<\/value> \
+\t\t<value>2<\/value> \
 \t<\/property> \
 \t<property> \
 \t\t<name>dfs.permission.enabled<\/name> \
@@ -97,18 +95,20 @@ sed -i "s/<configuration>/& \
 \t<property> \
 \t\t<name>dfs.namenode.acls.enabled<\/name> \
 \t\t<value>true<\/value> \
-\t<\/property>/" /usr/local/hadoop/etc/hadoop/hdfs-site.xml
+\t<\/property> \ 
+/' /usr/local/hadoop/etc/hadoop/hdfs-site.xml
 
 # core-site.xml
-sed -i "s/<configuration>/& \
-\n\t<property> \
+sed -i 's/<configuration>/& \
+\t<property> \
 \t\t<name>dfs.default.name<\/name> \
-\t\t<value>hdfs://node-master:9000<\/value> \
-\t<\/property>/" /usr/local/hadoop/etc/hadoop/core-site.xml
+\t\t<value>hdfs:\/\/node-master:9000<\/value> \
+\t<\/property> \ 
+/' /usr/local/hadoop/etc/hadoop/core-site.xml
 
 # mapred-site.xml
-sed -i "s/<configuration>/& \
-\n\t<property> \
+sed -i 's/<configuration>/& \
+\t<property> \
 \t\t<name>mapreduce.framework.name<\/name> \
 \t\t<value>yarn<\/value> \
 \t<\/property> \
@@ -123,4 +123,5 @@ sed -i "s/<configuration>/& \
 \t<property> \
 \t\t<name>mapreduce.reduce.env<\/name> \
 \t\t<value>HADOOP_MAPRED_HOME=$HADOOP_HOME<\/value> \
-\t<\/property>/" /usr/local/hadoop/etc/hadoop/mapred-site.xml
+\t<\/property> \
+/' /usr/local/hadoop/etc/hadoop/mapred-site.xml
