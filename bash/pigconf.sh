632ae9a5a36b1fa9ce3e2b40a979bb9efab1ef25
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-09-20 22:29:44 +0530

    script to install and configure pig in hadoop

---
+++ bash/pigconf.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# File: pigconf.sh
+# Name: D.Saravanan
+# Date: 20/09/2020
+# Script to install and configure Apache Pig in Hadoop
+
+user='raman'
+
+# Nodes
+mnode='172.17.0.2'
+enode='172.17.0.5'
+nodes='172.17.0.3 172.17.0.4'
+
+for node in $mnode $enode $nodes
+do
+
+done
