850049005d7717f9c90649a878082b2d2aed0561
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-09-28 20:35:20 +0530

    script to install and configure pig in hadoop

--- pigconf.sh
+++ pigconf.sh
@@ -13,16 +13,29 @@ nodes='172.17.0.3 172.17.0.4'
 
 for ip in $mnode $enode $nodes
 do
-	ssh -p 22 $user@$ip << EOF
+	ssh $user@$ip << EOF
 
 	if [ $ip == $mnode ]
 	then
-
 		wget -c http://apachemirror.wuchna.com/pig/pig-0.17.0/pig-0.17.0.tar.gz -P /home/$user/Downloads/
 		tar -xzf /home/$user/Downloads/pig-0.17.0.tar.gz
 		mv /home/$user/pig-0.17.0/ /usr/local/hadoop/pig/
 
+		# copy pig to nodes
+		scp -r /usr/local/hadoop/pig/ $user@172.17.0.3:/usr/local/hadoop/pig/
+		scp -r /usr/local/hadoop/pig/ $user@172.17.0.4:/usr/local/hadoop/pig/
+
+		# copy pig to edge node
+		scp -r /usr/local/hadoop/pig/ $user@172.17.0.5:/usr/local/hadoop/pig/
 	fi
+
+	# .bashrc
+	sed -i '$ a export PIG_HOME=\/usr\/local\/hadoop\/pig \
+	\nexport PIG_CONF_DIR=\$PIG_HOME\/conf \
+	\nexport PIG_CLASSPATH=\$HADOOP_HOME\/conf \
+	\nexport PATH=\$PIG_HOME\/bin:\$PATH' /home/$user/.bashrc
+
+	source ~/.bashrc
 		
 	logout
 
