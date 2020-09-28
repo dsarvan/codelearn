d5c1c4d3688b2f366b7dde4bfdb61a3a10b87f82
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-09-28 21:02:15 +0530

    script to find all factors of an integer

---
+++ python/allfactor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# File: allfactor.py
+# Name: D.Saravanan
+# Date: 23/09/2020
+# Script to find all factors of an integer
+
+value = []
+
+def factors(N):
+    for n in range(1, N+1):
+        if N%n == 0:
+            value.append(n)
+
+    print('The factors of {} are {}'.format(N, value))
+
+if __name__=='__main__':
+
+    N = float(input('Enter number: '))
+
+    if N > 0 and N.is_integer():
+        factors(int(N))
+    else: 
+        print('Invalid input, enter a positive integer.')
