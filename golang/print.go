9f554abe1c10b69a641df40b7a33b89aa9ba7d92
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-09-29 20:43:04 +0530

    program to print a string

---
+++ golang/print.go
@@ -0,0 +1,12 @@
+// File: print.go
+// Name: D.Saravanan
+// Date: 29/09/2020
+// Program to print a string
+
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Institute of Mathematical Sciences")
+}
+
