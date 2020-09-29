36016fa21c65c641df6975aa8a9be94d0d06859f
Dayalan Saravanan <dayalsaravanan@gmail.com>
2020-09-29 20:58:50 +0530

    program to print the length of a string

---
+++ golang/lenstr.go
@@ -0,0 +1,14 @@
+// File: lenstr.go
+// Name: D.Saravanan
+// Date: 29/09/2020
+// Program to print the length of a string
+
+package main
+import "fmt"
+
+func main() {
+
+	var institute string = "Institute of Mathematical Sciences"
+	fmt.Println(institute)
+	fmt.Println(len(institute))
+}
