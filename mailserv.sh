eeeb662db2e9481b4cb96c9527a4d755521bddec
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-01-05 16:48:06 +0530

    script to count the mail services from a file

---
+++ mailserv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# File: mailserv.sh
+# Name: D.Saravanan
+# Date: 05/01/2021
+# Script to count the mail services from a file
+
+echo "Input file name is $1"
+
+services="hotmail.com gmail.com microsoft.com verizon.net yahoo.com"
+
+for service in $services
+do
+    echo "Count of $service: `grep -icw $service $1`"
+done
