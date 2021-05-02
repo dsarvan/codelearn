a5119b5e6b13f5de1eda0a37c1da993fa42970a3
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-05-02 22:54:19 +0530

    script to compute nth value of the fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+''' Program to compute nth value of the Fibonacci series '''
+
+def fibonacci(nval):
+    ''' fibonacci function '''
+    return 1 if nval in (1, 2) else fibonacci(nval-1) + fibonacci(nval-2)
+
+if __name__ == "__main__":
+    N = 10
+    value = fibonacci(N)
+    print("The value of the Fibonacci series (for n = %d) is %d." %(N, value))
