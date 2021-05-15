ebb5e6d82840d911aceb394bce2481d2b3b01852
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-05-15 20:29:08 +0530

    script to use yield and static type

---
+++ python/sumto.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# File: sumto.py
+# Name: D.Saravanan
+# Date: 14/05/2021
+# Script to use yeild and static type
+
+def numbers():
+    """ lazy function, it only
+    creates numbers as requested """
+    for num in range(1024):
+        yield num
+
+def sumto(number: int) -> int:
+    """ accept an integer value for the 
+    n parameter and return an integer result """
+    summ: int = 0
+    for num in numbers():
+        if num == number: break
+        summ += num
+    return summ
+
+print(sumto(15))
