5d25cec10576d6a46f7aeccfa7d4ff0eb31152be
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-05-25 22:27:20 +0530

    script for defining classes

---
+++ python/classdef.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# File: classdef.py
+# Name: D.Saravanan
+# Date: 25/05/2021
+# Script for defining classes 
+
+class Complex:
+    """ complex number """
+    def __init__(self, realpart, imagpart):
+        """ assign arguments """
+        self.real = realpart
+        self.imag = imagpart
+
+x = Complex(3.0, -4.5)
+print(x.real, x.imag)
