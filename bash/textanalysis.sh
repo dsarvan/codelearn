8d9aa96dea5adfceec8b23eb7044825166785ded
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-05-28 22:37:27 +0530

    replaced cat with less and used double quote argument

--- bash/textanalysis.sh
+++ bash/textanalysis.sh
@@ -4,5 +4,5 @@
 # Date: 28/05/2021
 # Script to analyse the text to count and sort the uniq words
 
-cat $1 | tr ' ' '\n' | tr -d '[:punct:]' | tr '[A-Z]' '[a-z]' | sort | \
+less "$1" | tr ' ' '\n' | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | sort | \
     uniq -c | sort -nr | less
