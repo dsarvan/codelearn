63d37e6f9a646d750e97c988a7723cb291d3416b
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-05-28 20:36:35 +0530

    script to find the executable files in the directory

--- execfile.sh
+++ execfile.sh
@@ -5,5 +5,11 @@
 # Script to find the executable files
 
 echo "List of executable files in the $1 directory:"
-find . -perm -u+x -type f
+
+# find all executable files in the current directory
+# ./execfile
+#find . -perm -u+x -type f
+
+# find all executable file in the directory given as argument
+# ./execfile.sh ~/Documents
 find $1 -perm -u+x -type f
