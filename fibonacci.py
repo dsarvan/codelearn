feca2a1d98053a6e3b875e2aa363f1bd7e1df355
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-05-31 17:31:25 +0530

    script to compute nth value of the Fibonacci series

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+# File: fibonacci.py
+# Name: D.Saravanan
+# Date: 31/05/2021
 
-''' Program to compute nth value of the Fibonacci series '''
+""" Script to compute nth value of the Fibonacci series """
 
 def fibonacci(nval):
-    ''' fibonacci function '''
+    """ fibonacci function """
     return 1 if nval in (1, 2) else fibonacci(nval-1) + fibonacci(nval-2)
 
 if __name__ == "__main__":
