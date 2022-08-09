c0340b1c7a7e4117d852989ff204067e008495d1
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-08-10 00:29:21 +0530

    script to print the byte value

--- byteobj.py
+++ byteobj.py
@@ -2,13 +2,11 @@
 # File: byteobj.py
 # Name: D.Saravanan
 # Date: 04/05/2021
-# Script to print the byte value
 
-def main():
-    ''' main function '''
-    byteobj = b'52s3a6'
+""" Script to print the byte value """
 
-    for byte in byteobj:
-        print(byte)
+# byte object
+BYT = b"52s3a6"
 
-main()
+for byte in BYT:
+    print(byte)
