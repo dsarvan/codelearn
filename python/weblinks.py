6bc2eee4d491132b37197815f0a3545d6bf4b2de
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-08-26 14:10:08 +0530

    script to get the list of links from a website

--- python/weblinks.py
+++ python/weblinks.py
@@ -2,21 +2,20 @@
 # File: weblinks.py
 # Name: D.Saravanan
 # Date: 17/01/2021
-# Script to get the list of links from a website
 
-import requests
-import re
+""" Script to get the list of links from a website """
+
 import argparse
+import re
+import requests
 
-parser = argparse.ArgumentParser(
-        description = "Get list of links from a website"
-        )
+parser = argparse.ArgumentParser(description="Get list of links from a website")
 
 parser.add_argument("url", nargs="?", help="URL", default=None)
 arguments = parser.parse_args()
-use_arguments = True if arguments.url is not None else False
+USE_ARGUMENTS = True if arguments.url is not None else False
 
-if use_arguments:
+if USE_ARGUMENTS:
     url = arguments.url
 else:
     while True:
