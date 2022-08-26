54586c4dbd5f86ef93cfa6024eb2606e40627bfd
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-08-26 11:04:24 +0530

    script that checks whether a number is prime

--- python/prime.py
+++ python/prime.py
@@ -7,26 +7,15 @@
 
 import math
 
+
 def prime(number):
-    """ function to check prime """
+    """function to check prime"""
     sqrt_number = math.sqrt(number)
-    for n in range(2, int(sqrt_number) + 1):
-        if (number/n).is_integer():
+    for index in range(2, int(sqrt_number) + 1):
+        if (number / index).is_integer():
             return False
-        return True
+    return True
 
-print(f'Check number(10,000,000) = {prime(10_000_000)}')
-print(f'Check number(10,000,019) = {prime(10_000_019)}')
 
-#def primevect(number):
-#    """ function to check prime with concept of vectorization """
-#    sqrt_number = math.sqrt(number)
-#    numbers = range(2, int(sqrt_number) + 1)
-#    for m in range(0, len(numbers), 5):
-#        result = (number/numbers[m:(m+5)]).is_integer()
-#        if any(result):
-#            return False
-#        return True
-#
-#print(f'Check number(10,000,000) = {primevect(10_000_000)}')
-#print(f'Check number(10,000,019) = {primevect(10_000_019)}')
+print(f"Check number(10,000,000) = {prime(10_000_000)}")
+print(f"Check number(10,000,019) = {prime(10_000_019)}")
