549dc41ed6cd38035d7cc158302238ecc5d28d44
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-08-31 04:29:31 +0530

    multiline in python

---
+++ rust/program04/multiline.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+more = """ many
+lines
+    whitespace
+    """
+print(more)
+
+escape = ''' blah """ escaped '
+newline
+\n
+test
+'''
+print(escape)
+
+simple_char = 'ca'
