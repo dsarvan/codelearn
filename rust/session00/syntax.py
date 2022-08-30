9e3ca9095322ca1c84f9cd6b9f090404d2bfc062
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-08-30 23:33:24 +0530

    basic python syntax

---
+++ rust/session00/syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"docstring for module"
+
+
+def multiply(x: int, y: int) -> int:
+    "docstring for function"
+    # one line comment
+    return x * y
+    """multiline comment explaining
+    that type hints using annotations are not
+    enforced but can be used by third party tools
+    such as type checkers, IDEs, linters, etc.
+    """
+
+
+if __name__ == "__main__":
+    z = multiply(5, 6)
+    print("result: {}".format(z))
+    txt = f"result: {z}"  # python >= 3.6
+    print(txt)
