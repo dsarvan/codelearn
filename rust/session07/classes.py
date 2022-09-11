560656d8cc0444164054bff35533772d11a4d13d
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-09-11 21:56:18 +0530

    classes in python

---
+++ rust/session07/classes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+class A:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class B:
+    members = []
+
+    def __init__(self, x):
+        self.members.append(x)
+
+
+if __name__ == "__main__":
+    a = A(1, 2)
+    a.x += 2
+    print("a.x", a.x)
+    print("a.y", a.y)
+    b1 = B(1)
+    b2 = B(2)
+    print(b1.members, b2.members)
