63dd56a6f256ae9f1e1b46aef368144288454eb8
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-09-17 22:30:20 +0530

    script to check the factorial of a number

---
+++ haskell/factorialcheck.hs
@@ -0,0 +1,15 @@
+-- File: factorialcheck.hs
+-- Name: D.Saravanan
+-- Date: 17/09/2022
+-- Script to check the factorial of a number
+
+module Main where
+
+factorial n = if n == 0 then 1 else n * factorial (n - 1)
+
+main = do 
+        putStrLn "What is 5! ?"
+        x <- readLn
+        if x == factorial 5
+            then putStrLn "You're right!"
+            else putStrLn "You're wrong!"
