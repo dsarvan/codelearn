3e35e3244a6b4ca37e501085a68487177961fd32
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-09-18 22:33:35 +0530

    python without error handling

---
+++ rust/session0A/nohandling.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+def multiply(first: str, second: str):
+    first = int(first)
+    second = int(second)
+    return first * second
+
+
+if __name__ == "__main__":
+    twenty = multiply("10", "2")
+    print(f"double is {twenty}")
+
+    tt = multiply("t", "2")
+    print(f"double is {tt}")
