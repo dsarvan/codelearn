d6611fabb20e1e8ccc4712700de85c616fdd3ecb
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-11-16 11:57:46 +0530

    set xtics pi format

--- clang/parallelwave.c
+++ clang/parallelwave.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <mpi.h>
-#define PI 3.141592653589793
+#define PI M_PI
 
 int main(int argc, char **argv) {
 
@@ -17,11 +17,6 @@ int main(int argc, char **argv) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-	int lproc = rank - 1;
-	if (lproc < 0) lproc = MPI_PROC_NULL;
-	int rproc = rank + 1;
-	if (rproc >= size) rproc = MPI_PROC_NULL;
 
 	unsigned N = 10000000;
 	double *xval = (double *) malloc(N * sizeof(double));
@@ -56,17 +51,17 @@ int main(int argc, char **argv) {
 
 		FILE *gnuplot = popen("gnuplot -persist", "w");
 
+		fprintf(gnuplot, "set terminal pngcairo font 'Palatino,12'\n");
+		fprintf(gnuplot, "set output 'parallelwave.png'\n");
 		fprintf(gnuplot, "set style line 1 linecolor rgb '#e50000'\n");
 		fprintf(gnuplot, "set linetype 1 linewidth 1\n");
-		fprintf(gnuplot, "set title 'Sine wave computed with mpi' font ',12'\n");
-		fprintf(gnuplot, "set yrange [-1.5:1.5]\n");
-		fprintf(gnuplot, "set autoscale xfixmin\n");
-		fprintf(gnuplot, "set autoscale xfixmax\n");
-		fprintf(gnuplot, "set xlabel 'x' font ',11'\n");
-		fprintf(gnuplot, "set ylabel 'f(x)' font ',11'\n");
-		fprintf(gnuplot, "set tics font ',10'\n");
-		fprintf(gnuplot, "set terminal pngcairo\n");
-		fprintf(gnuplot, "set output 'parallelwave.png'\n");
+		fprintf(gnuplot, "set title 'Sine wave computed with mpi'\n");
+		fprintf(gnuplot, "set yrange [-1.5:1.5]; set grid; set key off\n");
+		fprintf(gnuplot, "set autoscale xfixmin; set autoscale xfixmax\n");
+		fprintf(gnuplot, "set xlabel '{/OpenSymbol x}' font ',11'\n");
+		fprintf(gnuplot, "set ylabel '{/OpenSymbol f(x)}' font ',11'\n");
+		fprintf(gnuplot, "set tics scale 0.5 font ',10'\n");
+		fprintf(gnuplot, "set xtics pi format '%.0Ppi'\n");
 		fprintf(gnuplot, "plot '-' with lines ls 1\n");
 		for (int i = 0; i < N; i++)
 			fprintf(gnuplot, "%e %e\n", xval[i], rwave[i]);
