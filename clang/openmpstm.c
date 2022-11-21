f783b6dc3fe89a6c5b3be3d9fad9e7202b1bd7b0
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-11-22 05:07:09 +0530

    adding a masked pragma to print output

---
+++ clang/openmpstm.c
@@ -0,0 +1,30 @@
+/* File: openmpstm.c
+ * Name: D.Saravanan
+ * Date: 20/11/2022
+ * A parallel OpenMP program that implements multiple threads
+ * Adding a masked pragma to print output
+*/
+
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+
+	printf("Goodbye slow serial world and Hello OpenMP!\n");
+
+	/* Adding a parallel region */
+	#pragma omp parallel /* Spawn threads */
+	{
+		/* Variables defined in a parallel region are private */
+		int nthreads = omp_get_num_threads();
+		int thread_id = omp_get_thread_num();
+
+		#pragma omp master /* directive to run only on main thread */
+		{
+			/* Places output statements into an OpenMP masked pragma block */
+			printf("I have %d thread(s) and my thread id is %d\n", nthreads, thread_id);
+		} /* Impiled Barrier */
+	} /* Implied Barrier */
+
+	return 0;
+}
