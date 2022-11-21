a1b4277477e9f4f57cc1e777143eba2d2de28742
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-11-21 15:45:42 +0530

    a serial openmp program that implements single thread

--- clang/openmps.c
+++ clang/openmps.c
@@ -5,14 +5,14 @@
 */
 
 #include <stdio.h>
-#include <omp.h>
+#include <omp.h> /* Includes OpenMP header file for the OpenMP function calls */
 
 int main(int argc, char *argv[]) {
 
 	int nthreads, thread_id;
 
-	nthreads = omp_get_num_threads();
-	thread_id = omp_get_thread_num();
+	nthreads = omp_get_num_threads(); /* number of threads */
+	thread_id = omp_get_thread_num(); /* thread ID */
 	printf("Goodbye slow serial world and Hello OpenMP!\n");
 	printf("I have %d thread(s) and my thread id is %d\n", nthreads, thread_id);
 
