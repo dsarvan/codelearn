b873be105115c802901183daa6f4761c4b29f48c
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-03-01 14:47:15 +0530

    remove file textanalysis.sh

--- textanalysis.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# File: textanalysis.sh
-# Name: D.Saravanan
-# Date: 28/05/2021
-# Script to analyse the text to count and sort the uniq words
-
-less "$1" | tr ' ' '\n' | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | sort | \
-    uniq -c | sort -nr | less
