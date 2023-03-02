6a18a7b8f8b8e46d3793dd938a859c26e6fbc30b
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-03-02 21:11:09 +0530

    script to find combination C(n,r)

---
+++ bash/combinationncr.sh
@@ -0,0 +1,46 @@
+# File: combinationncr.sh
+# Name: D.Saravanan    
+# Date: 24/02/2020
+# Script to find combination C(n,r)
+
+echo "Enter value n: "
+read n
+
+echo "Enter value r: "
+read r
+
+if [ $n -lt $r ]
+then
+	echo "Error: The value of n should be greater than or equal to r."
+	echo "Enter value n: "
+	read n
+fi
+
+
+diff=`expr $n - $r`
+
+x=1
+
+nvalue=1
+while [ $n -ge $x ]
+do
+	nvalue=`expr $nvalue \* $n`
+	((n--))
+done
+
+rvalue=1
+while [ $r -ge $x ]
+do
+	rvalue=`expr $rvalue \* $r`
+	((r--))
+done
+
+diffvalue=1
+while [ $diff -ge $x ]
+do
+	diffvalue=`expr $diffvalue \* $diff`
+	((diff--))
+done
+
+nCr=`expr "$nvalue/$rvalue * $diffvalue" | bc`
+echo "The number of combinations is $nCr."
