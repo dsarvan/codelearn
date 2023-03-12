75b71d27b69306fe26a20782148b053b6453615e
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-03-12 20:08:00 +0530

    program to evaluate a num linearly spaced intervals

--- clang/linspace.c
+++ clang/linspace.c
@@ -8,6 +8,37 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+double linspace(int N, double a, double b) {
+
+	double h = (b - a)/N;
+
+	double *x = (double *) calloc(N+1, sizeof(*x));
+	double *f = (double *) calloc(N+1, sizeof(*f));
+
+	for (size_t n = 0; n <= N; n++) {
+		x[n] = a + (b - a) * n / N;
+		f[n] = sin(x[n]);
+	}
+
+	free(x);
+
+	double sum(double f[], int nval) {
+		double sval = 0.0;
+		for (size_t i = 1; i <= nval; i++)
+			sval += f[i];
+		return sval;
+	}
+
+	double trap;
+	trap = (h/2) * (f[0] + 2 * sum(f, N-1) + f[N]);
+
+	free(f);
+
+	return trap;
+}
+
+
 double trapezoid(int N, double a, double b) {
 
 	double h = (b - a)/N;
@@ -33,6 +64,8 @@ double trapezoid(int N, double a, double b) {
 	double trap;
 	trap = (h/2) * (f[0] + 2 * sum(f, N-1) + f[N]);
 
+	free(f);
+
 	return trap;
 }
 
@@ -41,10 +74,10 @@ int main() {
 
 	int n = 100000000;	/* number of trapezoids */
 	double a = 0.0;		/* left endpoint */
-	double b = M_PI;	/* right endpoint */
+	double b = 10.0;	/* right endpoint */
 
-	double integral = trapezoid(n, a, b);
-	printf("%f\n", integral); 
+	printf("%f\n", linspace(n, a, b));
+	printf("%f\n", trapezoid(n, a, b));
 
 	return 0;
 }
