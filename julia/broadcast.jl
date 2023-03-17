bad8513810a9241a3b4741e015601ff1f3b9a213
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-03-17 19:12:14 +0530

    script to broadcast data to all processes

---
+++ julia/broadcast.jl
@@ -0,0 +1,40 @@
+#!/usr/bin/env julia
+# File: broadcast.jl
+# Name: D.Saravanan
+# Date: 07/03/2023
+
+""" Script to broadcast data to all processes """
+
+import MPI
+MPI.Init()
+
+function main()
+
+    comm = MPI.COMM_WORLD
+    rank = MPI.Comm_rank(comm)
+    size = MPI.Comm_size(comm)
+
+    if rank == 0
+        data = Dict(
+            "key1" => [10, 10.1, 10 + 11im],
+            "key2" => ["mpi-julia", "julia"],
+            "key3" => Array([1, 2, 3]),
+        )
+    else
+        data = 0
+    end
+
+    # broadcast data to all processes
+    data = MPI.bcast(data, root = 0, comm)
+
+    if rank == 0
+        println("bcast finished")
+    end
+
+    println("data on rank $rank is $data")
+
+    MPI.Barrier(comm)
+    MPI.Finalize()
+end
+
+main()
