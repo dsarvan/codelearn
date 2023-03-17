c4c74b751ef224c1c5b956738300899551df6106
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-03-17 19:07:56 +0530

    script to broadcast data to all processes

---
+++ python/broadcast.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# File: broadcast.py
+# Name: D.Saravanan
+# Date: 07/03/2023
+
+""" Script to broadcast data to all processes """
+
+from numpy import array
+from mpi4py import MPI
+
+
+def main():
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    if rank == 0:
+        data = {
+            "key1": [10, 10.1, 10 + 11j],
+            "key2": ["mpi4py", "python"],
+            "key3": array([1, 2, 3]),
+        }
+    else:
+        data = None
+
+    # broadcast data to all processes
+    data = comm.bcast(data, root=0)
+
+    if rank == 0:
+        print("bcast finished")
+
+    print(f"data on rank {rank} is {data}")
+    comm.Barrier()
+
+
+if __name__ == "__main__":
+    main()
