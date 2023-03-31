6431e359b08b9e9873beb4392fab32d4194d6ef4
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-03-31 20:28:52 +0530

    reformat script

--- python/devicequery.py
+++ python/devicequery.py
@@ -3,6 +3,7 @@
 # Name: D.Saravanan
 # Date: 11/03/2023
 
+""" Script to find number of gpu devices on host computer """
 
 import pycuda.driver as drv
 import pycuda.autoinit
@@ -11,21 +12,20 @@ import pycuda.autoinit
 print(f"Detected {drv.Device.count()} CUDA capable devices.")
 
 for n in range(drv.Device.count()):
+    device = drv.Device(n)
+    print(f"Device {n}: {device.name()}")
 
-	device = drv.Device(n)
-	print(f"Device {n}: {device.name()}")
+    compute = float("%d.%d" % device.compute_capability())
+    print(f"Compute capability: {compute}")
 
-	compute = float("%d.%d" % device.compute_capability())
-	print(f"Compute capability: {compute}")
+    print(f"Total Memory: {device.total_memory()//(1024**2)} megabytes")
 
-	print(f"Total Memory: {device.total_memory()//(1024**2)} megabytes")
+    # attributes of gpu device
+    device_attributes_tuples = device.get_attributes().iteritems()
+    device_attributes = {}
 
-	# attributes of gpu device
-	device_attributes_tuples = device.get_attributes().iteritems()
-	device_attributes = {}
+    for k, v in device_attributes_tuples:
+        device_attributes[str(k)] = v
 
-	for k, v in device_attributes_tuples:
-		device_attributes[str(k)] = v
-
-	sm = device_attributes["MULTIPROCESSOR_COUNT"]
-	print(sm)
+    sm = device_attributes["MULTIPROCESSOR_COUNT"]
+    print(sm)
