c3f74ac2a76517286dad51fb379c52c7babc3553
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-05-10 19:32:10 +0530

    script to create sawtooth wave by summing sine wave

--- python/sawtoothwave.py
+++ python/sawtoothwave.py
@@ -6,40 +6,55 @@
 """ Script to create sawtooth wave by summing sine wave """
 
 import numpy as np
-import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 plt.style.use("classic")
-plt.rcParams["text.usetex"] = True
-plt.rcParams["pgf.texsystem"] = "pdflatex"
-plt.rcParams.update(
-    {
-        "font.family": "serif",
-        "font.size": 8,
-        "axes.labelsize": 10,
-        "axes.titlesize": 10,
-        "figure.titlesize": 10,
-    }
-)
+plt.rc("text", usetex="True")
+plt.rc("figure", titlesize=10)
+plt.rc("pgf", texsystem="pdflatex")
+plt.rc("axes", labelsize=10, titlesize=10)
+plt.rc("font", family="serif", weight="normal", size=8)
 
-freq = 5000
-time = np.linspace(0, 2, freq)
-sawtooth_wave = np.sin(2*np.pi*time)
 
-fwriter = animation.writers["ffmpeg"]
-data = dict(title="Sawtooth wave animation")
-writer = fwriter(fps=15, metadata=data)
+def formatfunc(value, tick_number):
+    """set major axis formatter"""
+    nval = int(np.round(2 * value / np.pi))
+    if nval == 0:
+        return r"$0$"
+    if nval % 2 > 0 or nval % 2 < 0:
+        return r"${0}\pi/2$".format(nval)
+    return r"${0}\pi$".format(nval // 2)
 
-fig, ax = plt.subplots()
-(line1,) = ax.plot(sawtooth_wave, "r", lw=1)
 
-with writer.saving(fig, "sawtoothwave.mp4", 120):
+if __name__ == "__main__":
+    xval = np.linspace(-2 * np.pi, 2 * np.pi, 5000, endpoint=True)
+    sawtooth_wave = np.sin(xval)
 
-    line1.set_ydata(sawtooth_wave)
-    writer.grab_frame()
+    fwriter = animation.writers["ffmpeg"]
+    data = {"title": "Sawtooth wave animation"}
+    writer = fwriter(fps=15, metadata=data)
 
-    for n in range(2, 10001, 1):
-        sawtooth_wave += (-1)**(n-1) * (1/n) * np.sin(2*np.pi*n*time)
+    fig, ax = plt.subplots()
+    (line1,) = ax.plot(xval, sawtooth_wave, c="#363737", lw=1)
+    ax.spines[["left", "bottom"]].set_position(("data", 0))
+    ax.spines[["right", "top"]].set_visible(False)
+    ax.set_yticks([-2.5, -2, -1.5, -1, -0.5, 0.5, 1, 1.5, 2, 2.5])
+    ax.xaxis.set_major_locator(plt.MultipleLocator(np.pi / 2))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(np.pi / 4))
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(formatfunc))
+    ax.xaxis.set_ticks_position("bottom")
+    ax.yaxis.set_ticks_position("left")
+    ax.tick_params(which="major", direction="inout")
+    ax.set(xlim=(-2 * np.pi, 2 * np.pi), ylim=(-2.5, 2.5))
+    ax.set_title(r"Sawtooth wave by summing sine wave", pad=20)
 
+    with writer.saving(fig, "sawtoothwave.mp4", dpi=300):
         line1.set_ydata(sawtooth_wave)
         writer.grab_frame()
+
+        for n in range(2, 10001, 1):
+            sawtooth_wave += (-1) ** (n - 1) * (1 / n) * np.sin(n * xval)
+
+            line1.set_ydata(sawtooth_wave)
+            writer.grab_frame()
