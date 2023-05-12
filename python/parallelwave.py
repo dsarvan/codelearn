480abb9e3cf8c26f69c7e74a2119e8d7cc621296
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-05-12 19:32:10 +0530

    reset major axis formatter

--- python/parallelwave.py
+++ python/parallelwave.py
@@ -5,39 +5,29 @@
 
 """ Script to compute sine wave with mpi4py """
 
-from mpi4py import MPI
 import numpy as np
+from mpi4py import MPI
 import matplotlib.pyplot as plt
 
 plt.style.use("classic")
 plt.rc("text", usetex="True")
-plt.rc("pgf", texsystem="pdflatex")
-plt.rc("font", family="serif", weight="normal", size=10)
-plt.rc("axes", labelsize=12, titlesize=12)
 plt.rc("figure", titlesize=12)
+plt.rc("pgf", texsystem="pdflatex")
+plt.rc("axes", labelsize=12, titlesize=12)
+plt.rc("font", family="serif", weight="normal", size=10)
 
 
 def formatfunc(value, tick_number):
-    N = int(np.round(2 * value / np.pi))
-    if N == 0:
+    """set major axis formatter"""
+    nval = int(np.round(2 * value / np.pi))
+    if nval == 0:
         return r"$0$"
-    elif N == 1:
-        return r"$\pi/2$"
-    elif N == -1:
-        return r"$-\pi/2$"
-    elif N == 2:
-        return r"$\pi$"
-    elif N == -2:
-        return r"$-\pi$"
-    elif N % 2 > 0 or N % 2 < 0:
-        return r"${0}\pi/2$".format(N)
-    else:
-        N = N // 2
-        return r"${0}\pi$".format(N)
+    if nval % 2 > 0 or nval % 2 < 0:
+        return r"${0}\pi/2$".format(nval)
+    return r"${0}\pi$".format(nval // 2)
 
 
 def main():
-
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
