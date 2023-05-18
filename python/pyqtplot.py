b79ac2467d6d49d92950d979a3182f2eff9051cc
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-05-18 19:32:10 +0530

    create a plot widget with pyqtgraph

---
+++ python/pyqtplot.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# File: pyqtplot.py
+# Name: D.Saravanan
+# Date: 16/05/2023
+
+""" Script to plotting with PyQtGraph """
+
+import sys
+
+import pyqtgraph as pg
+from PyQt6 import QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    """Subclass of QMainWindow to customize application's main window."""
+
+    def __init__(self):
+        super().__init__()
+
+        # set the central widget of the window
+        self.graphWidget = pg.PlotWidget()
+        self.setCentralWidget(self.graphWidget)
+
+        hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
+
+        # plot data: x, y values
+        self.graphWidget.plot(hour, temperature)
+
+
+def main():
+    """Need one (and only one) QApplication instance per application.
+    Pass in sys.argv to allow command line arguments for the application.
+    If no command line arguments than QApplication([]) is required."""
+    app = QtWidgets.QApplication(sys.argv)
+
+    window = MainWindow()  # create a subclass of QMainWindow
+    window.show()  # windows are hidden by default
+
+    sys.exit(app.exec())  # start the event loop
+
+
+if __name__ == "__main__":
+    main()
