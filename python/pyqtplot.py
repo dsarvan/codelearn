36a0c59600abbe1ee35a16fd6a91f6e244573c65
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-05-26 19:32:10 +0530

    set the background to magenta, by passing in the string "m"

--- python/pyqtplot.py
+++ python/pyqtplot.py
@@ -29,8 +29,8 @@ class MainWindow(QtWidgets.QMainWindow):
         hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
 
-        # set the background to cyan, by passing in the string "c"
-        self.graphWidget.setBackground("c")
+        # set the background to magenta, by passing in the string "m"
+        self.graphWidget.setBackground("m")
 
         # plot data: x, y values
         self.graphWidget.plot(hour, temperature)
