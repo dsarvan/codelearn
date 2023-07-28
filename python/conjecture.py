262a6a18c85c4975da185a503747817909e66671
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-07-28 19:32:10 +0530

    script for 3n+1 collatz conjecture with type annotations

--- python/conjecture.py
+++ python/conjecture.py
@@ -1,18 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # File: conjecture.py
 # Name: D.Saravanan
 # Date: 13/07/2021
 
-""" Script for 3N + 1 conjecture """
+""" Script for 3n + 1 collatz conjecture """
 
-N: int = 7
-print(N)
 
-while N != 1:
+def conjecture(n: int) -> list[int]:
+    """collatz conjecture"""
+    sequence: list[int] = [n]
 
-    if N % 2 == 0:
-        N = N / 2
-    else:
-        N = (3 * N) + 1
+    while n != 1:
+        n = n // 2 if n % 2 == 0 else (3 * n) + 1
+        sequence = sequence + [n]
 
-    print(int(N))
+    return sequence
+
+
+if __name__ == "__main__":
+    print(conjecture(27))
