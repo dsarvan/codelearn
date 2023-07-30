670041b58161dc31bc50ddf371a6fc2183553bb1
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-07-30 19:32:10 +0530

    script to find all factors of a positive integer

--- python/allfactor.py
+++ python/allfactor.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # File: allfactor.py
 # Name: D.Saravanan
 # Date: 23/09/2020
 
-""" Script to find all factors of an integer """
+""" Script to find all factors of a positive integer """
 
 
-def factors(nval: int) -> list:
+def factors(nval: int) -> list[int]:
     """factors of an integer"""
     return [n for n in range(1, nval + 1) if nval % n == 0]
 
 
 if __name__ == "__main__":
+    while True:
+        NUM: int = int(input("Enter a positive integer: "))
 
-    NUM = float(input("Enter number: "))
+        if NUM >> 31 == 0:
+            print(f"The factors of {NUM} are {factors(NUM)}")
+            break
 
-    if NUM > 0 and NUM.is_integer():
-        NUM = int(NUM)
-        print(f"The factors of {NUM} are {factors(NUM)}")
-    else:
         print("Invalid input, enter a positive integer.")
