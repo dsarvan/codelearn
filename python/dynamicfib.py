25e27615f25cb87888e5b98b9a30cbd87eea3a92
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-08-18 19:32:10 +0530

    script to compute nth fibonacci using dynamic programming

--- python/dynamicfib.py
+++ python/dynamicfib.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
-# File: fibonaccid.py
+# File: dynamicfib.py
 # Name: D.Saravanan
 # Date: 29/10/2022
 
-""" Solving standard fibonacci with dynamic programming """
+""" Script to compute nth fibonacci using dynamic programming """
 
-def fibonacci(n):
-    fib = [0, 1, None]
-    if n < 0:
-        return "Invalid input"
-    elif n == 0:
-        return fib[0]
-    elif n == 1:
-        return fib[1]
-    else:
-        for _ in range(2, n+1):
-            fib[-1] = fib[0] + fib[1]
-            fib[0] = fib[1]
-            fib[1] = fib[-1]
-        return fib[-1]
+
+def fibonacci(n: int) -> int:
+    """compute nth fibonacci"""
+
+    fib: list[int] = [0, 1]
+
+    for _ in range(n):
+        fib[0] = fib[0] + fib[1]
+        fib[0], fib[1] = fib[1], fib[0]
+
+    return fib[0]
 
 
 if __name__ == "__main__":
-    N = 10
+    N: int = 10000
     print(fibonacci(N))
