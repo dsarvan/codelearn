e72a66848b06053086a494377958c695b9990edd
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-11-08 19:32:10 +0530

    mpi program working example

---
+++ mpiprog/clang/mpiprog01.c
@@ -0,0 +1,33 @@
+/* File: mpiprog01.c
+ * Name: D.Saravanan
+ * Date: 08/11/2022
+ * MPI program working example
+*/
+
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+
+    int rank, size, length;
+    char name[MPI_MAX_PROCESSOR_NAME];
+
+    /* Initializes after program start, including program arguments */
+    MPI_Init(&argc, &argv);
+
+    /* Gets the rank number of the process */
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    /* Gets the number of ranks in the program determined by the mpiexec command */
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    /* Gets the name of the processor */
+    MPI_Get_processor_name(name, &length);
+
+    printf("rank %d of nproc %d on %s\n", rank, size, name);
+
+    /* Finalizes MPI to synchronize ranks and then exits */
+    MPI_Finalize();
+
+    return 0;
+}
