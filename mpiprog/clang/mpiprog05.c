1ceb728da2ff10da19b79f22f639607f64fc5405
Saravanan Dayalan <dayalsaravanan@gmail.com>
2023-12-04 19:32:45 +0530

    program to initialize matrices, assign values, demonstrates data decomposition and compute matrix multiplication

--- mpiprog/clang/mpiprog05.c
+++ mpiprog/clang/mpiprog05.c
@@ -13,9 +13,9 @@ int main(int argc, char **argv) {
 
 	int rank, size;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	MPI_Status status;
 	MPI_Request request;
