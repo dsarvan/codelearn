41ccd0e03de578d61699fbf785f078888fabf27f
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-01-09 19:32:10 +0530

    script to display number of persons in a given age group

--- bash/age.sh
+++ bash/age.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 # File: age.sh
-# Name: D.Saravanan    
+# Name: D.Saravanan
 # Date: 24/02/2020
 # Script to display number of persons in a given age group
 
 echo " "
 
-while read line
-do
+while read line; do
 	echo "$line"
-done < list.csv
+done <list.csv
 
-min=`cat list.csv | tail -n +2 | cut -d "," -f 3 | sort -n | head -n 1`
-max=`cat list.csv | tail -n +2 | cut -d "," -f 3 | sort -n | tail -n 1`
+min=$(cat list.csv | tail -n +2 | cut -d "," -f 3 | sort -n | head -n 1)
+max=$(cat list.csv | tail -n +2 | cut -d "," -f 3 | sort -n | tail -n 1)
 
 echo " "
 
@@ -21,8 +20,7 @@ read lower
 
 echo " "
 
-if [ $lower -lt $min ]
-then
+if [ $lower -lt $min ]; then
 	echo "The minimum age value in the list is $min."
 	echo "Enter lower limit age: "
 	read lower
@@ -31,8 +29,7 @@ fi
 echo "Enter upper limit age: "
 read upper
 
-if [ $upper -gt $max ]
-then
+if [ $upper -gt $max ]; then
 	echo "The maximum age value in the list is $max."
 	echo "Enter upper limit age: "
 	read upper
@@ -46,12 +43,10 @@ count=0
 #sorted_age_list=`cat list.csv | tail -n +2 | cut -d "," -f 3 | sort -n`
 #echo $sorted_age_list
 
-for i in {1..20}
-do
-	age[i]=`cat list.csv | tail -n +2 | cut -d "," -f 3` 
+for i in {1..20}; do
+	age[i]=$(cat list.csv | tail -n +2 | cut -d "," -f 3)
 
-	if [ ${age[i]} -ge $lower ] && [ ${age[i]} -le $upper ];
-	then
+	if [ ${age[i]} -ge $lower ] && [ ${age[i]} -le $upper ]; then
 		((count++))
 	fi
 done
