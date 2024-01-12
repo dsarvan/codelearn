663b2369c6073edf862febd8522a10a1f7e0e3ad
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-01-12 19:32:10 +0530

    script to find combination C(n,r)

--- bash/combinationncr.sh
+++ bash/combinationncr.sh
@@ -1,5 +1,5 @@
 # File: combinationncr.sh
-# Name: D.Saravanan    
+# Name: D.Saravanan
 # Date: 24/02/2020
 # Script to find combination C(n,r)
 
@@ -9,38 +9,33 @@ read n
 echo "Enter value r: "
 read r
 
-if [ $n -lt $r ]
-then
+if [ $n -lt $r ]; then
 	echo "Error: The value of n should be greater than or equal to r."
 	echo "Enter value n: "
 	read n
 fi
 
-
-diff=`expr $n - $r`
+diff=$(expr $n - $r)
 
 x=1
 
 nvalue=1
-while [ $n -ge $x ]
-do
-	nvalue=`expr $nvalue \* $n`
+while [ $n -ge $x ]; do
+	nvalue=$(expr $nvalue \* $n)
 	((n--))
 done
 
 rvalue=1
-while [ $r -ge $x ]
-do
-	rvalue=`expr $rvalue \* $r`
+while [ $r -ge $x ]; do
+	rvalue=$(expr $rvalue \* $r)
 	((r--))
 done
 
 diffvalue=1
-while [ $diff -ge $x ]
-do
-	diffvalue=`expr $diffvalue \* $diff`
+while [ $diff -ge $x ]; do
+	diffvalue=$(expr $diffvalue \* $diff)
 	((diff--))
 done
 
-nCr=`expr "$nvalue/$rvalue * $diffvalue" | bc`
+nCr=$(expr "$nvalue/$rvalue * $diffvalue" | bc)
 echo "The number of combinations is $nCr."
