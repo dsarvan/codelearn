c440e4813ad61c21e6e2d18f7da9b2661bd38ae8
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-01-14 19:32:10 +0530

    script to read user input and write/append to a file

--- bash/empscript.sh
+++ bash/empscript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # File: empscript.sh
-# Name: D.Saravanan    
+# Name: D.Saravanan
 # Date: 24/02/2020
 # Script to read user input and write/append to a file
 
@@ -9,18 +9,17 @@ touch empfile.txt
 
 # write header (EmpID, EmpName, Basic) to empfile.txt
 #echo "EmpID | EmpName | Basic" > empfile.txt
-printf "%4s\t%20s\t%8s\n" "EmpID | EmpName | Basic" > empfile.txt 
+printf "%4s\t%20s\t%8s\n" "EmpID | EmpName | Basic" >empfile.txt
 
 # ask how many number of records to be entered
 echo "Enter number of records: "
 read n
 
-i=1         # initialize i = 1
-empid=100   # initialize empid = 100
+i=1       # initialize i = 1
+empid=100 # initialize empid = 100
 
 # start of while loop
-while [ $i -le $n ]
-do
+while [ $i -le $n ]; do
 	((empid++)) # auto increment empid by 1
 	echo "Enter name: "
 	read empname
@@ -28,7 +27,7 @@ do
 	read basic
 
 	#echo "$empid | $empname | $basic" >> empfile.txt
-	printf "%4s\t%20s\t%8s\n" "$empid | $empname | $basic" >> empfile.txt
+	printf "%4s\t%20s\t%8s\n" "$empid | $empname | $basic" >>empfile.txt
 
-	((i++))  # increment i by 1
+	((i++)) # increment i by 1
 done
