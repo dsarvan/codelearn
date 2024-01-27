82da74826952211b487ce6229e1def46ce3fa0b8
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-01-27 19:32:10 +0530

    format script with shfmt

--- bash/msgcheck.sh
+++ bash/msgcheck.sh
@@ -2,8 +2,7 @@
 
 echo "This scripts checks the existence of the messages file."
 echo "Checking..."
-if [ -f /var/log/messages ]
-then
+if [ -f /var/log/messages ]; then
 	echo "/var/log/messages exists."
 fi
 echo
