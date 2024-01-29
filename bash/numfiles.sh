09ae606b723f0a8dbccc6fa429e896916d48ab8f
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-01-29 19:32:10 +0530

    script to find number of files in pwd

--- bash/numfiles.sh
+++ bash/numfiles.sh
@@ -5,12 +5,10 @@
 
 number_of_files="$(find . * | wc -l)"
 
-if [[ "$number_of_files" -gt 100 ]]
-then
-    echo "More than 100 files in the working directory!"
-elif [[ "$number_of_files" -gt 10 ]]
-then
-    echo "More than 10 files in the working directory!"
+if [[ "$number_of_files" -gt 100 ]]; then
+	echo "More than 100 files in the working directory!"
+elif [[ "$number_of_files" -gt 10 ]]; then
+	echo "More than 10 files in the working directory!"
 else
-    echo "Ten or fewer files in the working directory."
+	echo "Ten or fewer files in the working directory."
 fi
