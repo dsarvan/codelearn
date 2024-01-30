828dcc30751a2c807da577a9f871685b6e84ccbc
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-01-30 19:32:10 +0530

    script to generate 12 character passwords

--- bash/passgen.sh
+++ bash/passgen.sh
@@ -4,6 +4,6 @@
 
 echo 'Generating 12-character passwords'
 
-for ((n=0;n<12;n++))
-do dd if=/dev/urandom count=1 2> /dev/null | uuencode -m - | sed -ne 2p | cut -c-12
+for ((n = 0; n < 12; n++)); do
+	dd if=/dev/urandom count=1 2>/dev/null | uuencode -m - | sed -ne 2p | cut -c-12
 done
