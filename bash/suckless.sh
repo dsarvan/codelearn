5dac378fb5a7dce7868dfadb75331c778068828f
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-03 19:32:10 +0530

    script to install suckless programs

--- bash/suckless.sh
+++ bash/suckless.sh
@@ -8,21 +8,19 @@ export OS=$(uname -s)
 
 suckless() {
 
-    program=$1
+	program=$1
 
-    if [ -d $program ]
-    then
-        cd $program
-        git pull
-    else
-        git clone https://git.suckless.org/$program
-        cd $program
-    fi
+	if [ -d $program ]; then
+		cd $program
+		git pull
+	else
+		git clone https://git.suckless.org/$program
+		cd $program
+	fi
 
-    cd
+	cd
 }
 
-for program in dmenu dwm slock slstatus st surf
-do
-    suckless $program
+for program in dmenu dwm slock slstatus st surf; do
+	suckless $program
 done
