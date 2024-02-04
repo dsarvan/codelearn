253154d404a0f9bafdadcfbb7b486b06760841eb
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-04 19:32:10 +0530

    script to print info regarding system

--- bash/system.sh
+++ bash/system.sh
@@ -5,14 +5,14 @@ clear
 echo "Hi, $USER"
 echo
 
-echo "Today's date is `date`, this is week `date +"%V"`."
+echo "Today's date is $(date), this is week $(date +"%V")."
 echo
 
 echo "These users are currently connected:"
 w | cut -d " " -f 1 - | grep -v USER | sort -u
 echo
 
-echo "This is `uname -s` running on a `uname -m` processor."
+echo "This is $(uname -s) running on a $(uname -m) processor."
 echo
 
 echo "This is the uptime information:"
