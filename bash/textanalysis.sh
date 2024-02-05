ea37085d523baf6eb1e62efbf9976bbabc367f47
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-05 19:32:10 +0530

    format script with shfmt

--- bash/textanalysis.sh
+++ bash/textanalysis.sh
@@ -4,5 +4,5 @@
 # Date: 28/05/2021
 # Script to analyse the text to count and sort the uniq words
 
-less "$1" | tr ' ' '\n' | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | sort | \
-    uniq -c | sort -nr | less
+less "$1" | tr ' ' '\n' | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | sort |
+	uniq -c | sort -nr | less
