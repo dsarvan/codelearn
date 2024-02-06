fc535a7f74db895d6e78f1cb6089c032b0c1ea19
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-06 19:32:10 +0530

    script to display welcome message and date

--- bash/welcome.sh
+++ bash/welcome.sh
@@ -4,4 +4,4 @@
 # Date: 02/01/2021
 # Script to display welcome message and date
 
-echo "Welcome Saravanan Dayalan. Today's date is `date | cut -d ' ' -f 2,4,7`."
+echo "Welcome Saravanan Dayalan. Today's date is $(date | cut -d ' ' -f 2,4,7)."
