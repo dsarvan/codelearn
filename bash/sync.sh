a3d89271601cc03d05b2f54fe85ac72debe268b9
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-12 20:34:41 +0530

    script to sync files using rsync

---
+++ bash/sync.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# File: sync.sh
+# Name: D.Saravanan
+# Date: 11/02/2024
+# Script to sync files using rsync
+
+rsync -av .config/qutebrowser/bookmarks/urls .gusdt/home/user/.config/qutebrowser/bookmarks/urls
