a08bd0465e4d4218021ff14fbdfd9486b9588ed9
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-12 20:32:10 +0530

    script to install debian packages and suckless programs

---
+++ bash/debpkg.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# File: debpkg.sh
+# Name: D.Saravanan
+# Script to install debian packages and suckless programs
+
+packages='x11-xserver-utils git make gcc libx11-dev libxft-dev libxinerama-dev
+xorg unclutter scrot sxiv build-essential texlive-full zathura zathura-ps
+zathura-djvu vim ffmpeg mpv alsa-utils w3m pandoc qutebrowser indent'
+
+for package in $packages
+do
+    doas apt-get install -y $package
+done
+
+sucks='dwm st slstatus dmenu'
+
+for suck in $sucks
+do
+    git clone https://git.suckless.org/$suck
+done
