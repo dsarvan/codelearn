279472a4dca73ed90af6fc46a04993f99093de19
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-19 19:32:10 +0530

    script to compute array elements sum

---
+++ ctypes/computepy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# File: computepy.py
+# Name: D.Saravanan
+# Date: 19/02/2024
+
+""" Script to compute array elements sum """
+
+
+import numpy as np
+
+
+def sumval(arr):
+    """ summation """
+    return np.sum(arr)
