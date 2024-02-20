570e7790b0b034e4fae2dba836bda7208e57e9fb
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-20 18:21:28 +0530

    invoke task

--- ctypes/tasks.py
+++ ctypes/tasks.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python
 import invoke
 
-
-@invoke.task
-def scriptrun(c):
-	print("script run!")
-
 @invoke.task
 def program(c, path=None):
+	""" build shared library program.so """
 	invoke.run("gcc -O3 -c -Wall -Werror -fpic program.c")
 	invoke.run("gcc -shared -o program.so program.o")
 
 @invoke.task
 def matmult(c, path=None):
+	""" build shared library matmult.so """
 	invoke.run("mpicc -O3 -c -Wall -Werror -fpic matmult.c")
 	invoke.run("mpicc -shared -o matmult.so matmult.o")
 
 @invoke.task
 def laplace(c, path=None):
+	""" build shared library laplace.so """
 	invoke.run("gcc -O3 -c -Wall -Werror -lm -fpic laplace.c")
 	invoke.run("gcc -shared -o laplace.so laplace.o")
 
 @invoke.task
 def compute(c, path=None):
+	""" build shared library compute.so """
 	invoke.run("gcc -O3 -c -Wall -Werror -fpic compute.c")
 	invoke.run("gcc -shared -o compute.so compute.o")
 
 @invoke.task
 def clean(c):
+	""" remove built objects """
 	invoke.run("rm -f program.o matmult.o laplace.o compute.o")
 
 @invoke.task(program, matmult, laplace, compute)
 def all(c):
+	""" build all tasks """
 	pass
