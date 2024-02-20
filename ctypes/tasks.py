6fc384dd108a81345381dac75f0708fe7cfbd76e
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-20 23:59:48 +0530

    replace invoke.run() with ctx.run()

--- ctypes/tasks.py
+++ ctypes/tasks.py
@@ -2,35 +2,35 @@
 import invoke
 
 @invoke.task
-def program(c, path=None):
-	""" build shared library program.so """
-	invoke.run("gcc -O3 -c -Wall -Werror -fpic program.c")
-	invoke.run("gcc -shared -o program.so program.o")
+def program(ctx, path=None):
+    """build shared library program.so"""
+    ctx.run("gcc -O3 -c -Wall -Werror -fpic program.c")
+    ctx.run("gcc -shared -o program.so program.o")
 
 @invoke.task
-def matmult(c, path=None):
-	""" build shared library matmult.so """
-	invoke.run("mpicc -O3 -c -Wall -Werror -fpic matmult.c")
-	invoke.run("mpicc -shared -o matmult.so matmult.o")
+def matmult(ctx, path=None):
+    """build shared library matmult.so"""
+    ctx.run("mpicc -O3 -c -Wall -Werror -fpic matmult.c")
+    ctx.run("mpicc -shared -o matmult.so matmult.o")
 
 @invoke.task
-def laplace(c, path=None):
-	""" build shared library laplace.so """
-	invoke.run("gcc -O3 -c -Wall -Werror -lm -fpic laplace.c")
-	invoke.run("gcc -shared -o laplace.so laplace.o")
+def laplace(ctx, path=None):
+    """build shared library laplace.so"""
+    ctx.run("gcc -O3 -c -Wall -Werror -lm -fpic laplace.c")
+    ctx.run("gcc -shared -o laplace.so laplace.o")
 
 @invoke.task
-def compute(c, path=None):
-	""" build shared library compute.so """
-	invoke.run("gcc -O3 -c -Wall -Werror -fpic compute.c")
-	invoke.run("gcc -shared -o compute.so compute.o")
+def compute(ctx, path=None):
+    """build shared library compute.so"""
+    ctx.run("gcc -O3 -c -Wall -Werror -fpic compute.c")
+    ctx.run("gcc -shared -o compute.so compute.o")
 
 @invoke.task
-def clean(c):
-	""" remove built objects """
-	invoke.run("rm -f program.o matmult.o laplace.o compute.o")
+def clean(ctx, path=None):
+    """remove built objects"""
+    ctx.run("rm -f program.o matmult.o laplace.o compute.o")
 
 @invoke.task(program, matmult, laplace, compute)
-def all(c):
-	""" build all tasks """
-	pass
+def all(ctx, path=None):
+    """build all tasks"""
+    pass
