225ba1c5391463ba660874bdc87d16c7ecb0cfc6
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-03-10 13:04:01 +0530

    script to install debian packages and suckless programs

--- bash/debpkg.sh
+++ bash/debpkg.sh
@@ -3,16 +3,18 @@
 # Name: D.Saravanan
 # Script to install debian packages and suckless programs
 
-packages='x11-xserver-utils git make gcc libx11-dev libxft-dev libxinerama-dev
-xorg unclutter scrot sxiv build-essential texlive-full zathura zathura-ps
-zathura-djvu vim ffmpeg mpv alsa-utils w3m pandoc qutebrowser indent'
-
-for package in $packages; do
-	doas apt-get install -y $package
-done
-
 sucks='dwm st slstatus dmenu'
 
 for suck in $sucks; do
-	git clone https://git.suckless.org/$suck
+	git clone https://git.suckless.org/"$suck"
 done
+
+packages='x11-xserver-utils git make gcc libx11-dev libxft-dev libxinerama-dev
+xorg unclutter scrot sxiv build-essential texlive-full zathura zathura-ps
+zathura-djvu vim ffmpeg mpv alsa-utils w3m pandoc qutebrowser indent wordnet'
+
+for package in $packages; do
+	doas apt-get install -y "$package"
+done
+
+doas apt-get remove -y wordnet-gui
