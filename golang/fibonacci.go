1bf07079c95355906cc10b7ce3a147978516e5d1
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-05-03 19:32:10 +0530

    reformat program with go fmt

--- golang/fibonacci.go
+++ golang/fibonacci.go
@@ -4,16 +4,17 @@
 // Program to generate Fibonacci sequence
 
 package main
+
 import "fmt"
 
 func fibonacci(n int) int {
-    x, y := 0, 1
+	x, y := 0, 1
 
-    for i := 0; i < n; i++ {
-        x, y = y, x+y
-    }
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+	}
 
-    return x
+	return x
 }
 
 func main() {
