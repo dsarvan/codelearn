bb71cd80525407c76e9754b3685f7844ee031028
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-05-08 19:32:10 +0530

    program to compute an svg rendering of 3 dimensional surface function

--- golang/surface.go
+++ golang/surface.go
@@ -40,12 +40,21 @@ func corner(i, j int) (float64, float64) {
 
 func main() {
 
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+
 	for i := 0; i <= cells; i++ {
 		for j := 0; j <= cells; j++ {
 			ax, ay := corner(i+1, j)
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
+
+			fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
+
+	fmt.Println("</svg>")
 }
