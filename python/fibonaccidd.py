cb177818d1d0da48508e5046555f332f1e366ed7
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-05-09 19:32:10 +0530

    solving standard fibonacci with dynamic programming

--- python/fibonaccidd.py
+++ python/fibonaccidd.py
@@ -4,23 +4,21 @@
 # Date: 29/10/2022
 
 """ Solving standard fibonacci with dynamic programming """
+# python -X int_max_str_digits=0 fibonaccid.py
 
-def fibonacci(n):
-    fib = [0, 1, None]
-    if n < 0:
-        return "Invalid input"
-    elif n == 0:
-        return fib[0]
-    elif n == 1:
-        return fib[1]
-    else:
-        for _ in range(2, n+1):
-            fib[-1] = fib[0] + fib[1]
-            fib[0] = fib[1]
-            fib[1] = fib[-1]
-        return fib[-1]
+
+def fibonacci(n: int) -> int:
+    """compute nth fibonacci"""
+
+    fib: list[int] = [0, 1, 1]
+
+    for _ in range(1, n):
+        fib[2] = fib[0] + fib[1]
+        fib[0], fib[1] = fib[1], fib[2]
+
+    return fib[0] if n == 0 else fib[1] if n == 1 else fib[2]
 
 
 if __name__ == "__main__":
-    N = 10
-    print(fibonacci(N))
+    N: int = 100000
+    print("Invalid input" if N < 0 else fibonacci(N))
