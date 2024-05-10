7de03a7357d69cc2280f4319b8573e0124268e4d
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-05-10 19:32:10 +0530

    compute nth fibonacci using recursion function

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # File: fibonacci.py
 # Name: D.Saravanan
 # Date: 31/05/2021
 
 """ Script to compute nth value of the Fibonacci series """
 
-def fibonacci(nval):
-    """ fibonacci function """
-    return 1 if nval in (1, 2) else fibonacci(nval-1) + fibonacci(nval-2)
+
+def fibonacci(nval: int) -> int:
+    """compute nth fibonacci"""
+    return 1 if nval in (1, 2) else fibonacci(nval - 1) + fibonacci(nval - 2)
+
 
 if __name__ == "__main__":
-    N = 10
-    value = fibonacci(N)
-    print("The value of the Fibonacci series (for n = %d) is %d." %(N, value))
+    N: int = 10
+    value: int = fibonacci(N)
+    print(f"The value of the Fibonacci series (for n = {N}) is {value}.")
